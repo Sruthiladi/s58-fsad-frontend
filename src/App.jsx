869b85a1dcfa6d70f6c40b9fb4b3d3947c597787
Sruthiladi <sruthiladi123@gmail.com>
2026-02-26 19:46:19 +0530

    responsive changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import './App.css'
 import Header from './components/Header'
 import Content from './components/Content'
 import Footer from './components/Footer'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
+
   return (
-    <div className='container'>
-      <Header />
-      <Content />
-      <Footer />
-    </div> 
+      <div className='container'>
+        <BrowserRouter>
+         <Header />
+         <Content />
+         <Footer />
+        </BrowserRouter>
+      </div>
   )
+
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import logo from '/image.png'
+import { Link } from 'react-router-dom'
+
 function Header() {
+
+  function toggleView() {
+    const systemView = document.querySelector('.systemview')
+    systemView.classList.toggle('mobileview')
+  }
+
   return (
     <div className='header'>
+
+      <div className='mobilemenu' onClick={toggleView}> &#9776; </div>
+
+      <ul className='systemview'>
+        <li> <Link to="/student" className='nav-link'>Student</Link> </li>
+        <li> <Link to="/signin" className='nav-link'>Signin</Link> </li>
+        <li> <Link to="/signup" className='nav-link'>Signup</Link> </li>
+      </ul>
+
       <img src={logo} alt="Logo" />
-        <h3>My Website Header</h3>
+      <h3>Website Heading</h3>
     </div>
   )
 }
